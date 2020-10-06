21a7d5b10cfb21f5dedfb3591f78774b1fb45027
Carlos Machado <carlosmachado@edu.univali.br>
2020-10-06 16:25:42 -0300

    Initial commit

---
+++ src/me/carlosmachado/tokenizer/Main.java
@@ -0,0 +1,34 @@
+package me.carlosmachado.tokenizer;
+
+import javax.swing.UIManager;
+import me.carlosmachado.tokenizer.front.MainForm;
+
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainForm().setVisible(true);
+            }
+        });
+    }
+    
+}
---
+++ src/me/carlosmachado/tokenizer/back/Parser.java
@@ -0,0 +1,16 @@
+package me.carlosmachado.tokenizer.back;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Parser {
+    
+    public static List<String> parse(String str){
+        List<String> tokens = new ArrayList<>();
+        
+        
+        
+        return tokens;
+    }
+    
+}
